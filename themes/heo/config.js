76ed00b5706813d7e744cc479c1afddee607a94a
Martin19845566 <117623526+Martin19845566@users.noreply.github.com>
2024-02-23 17:01:20 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,10 +4,10 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  // HEO_NOTICE_BAR: [
+  //   { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+  //   { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  // ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -104,7 +104,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
@@ -113,9 +113,9 @@ const CONFIG = {
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
   HEO_MENU_INDEX: true, // 显示首页
-  HEO_MENU_CATEGORY: true, // 显示分类
+  HEO_MENU_CATEGORY: false, // 显示分类
   HEO_MENU_TAG: true, // 显示标签
-  HEO_MENU_ARCHIVE: true, // 显示归档
+  HEO_MENU_ARCHIVE: false, // 显示归档
   HEO_MENU_SEARCH: true, // 显示搜索
 
   HEO_POST_LIST_COVER: true, // 列表显示文章封面
